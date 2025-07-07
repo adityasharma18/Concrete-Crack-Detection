1d3e8a1a5541e534278c296be4c82e67b21942fa
adityasharma18 <aditya.ceo2001@gmail.com>
2025-07-08 04:19:14 +0530

    Fixed app.run block for deployment

--- app.py
+++ app.py
@@ -46,8 +46,6 @@ def index():
     return render_template('index.html', result=result, show_graph=show_graph)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
